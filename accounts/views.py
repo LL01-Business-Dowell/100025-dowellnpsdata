58d9926c96ba935f4d376eb1b941a66adabc05d0
Ahsan Ali <mr.ahsanali2000@gmail.com>
2022-05-06 12:58:56 +0500

    reformated views file

--- accounts/views.py
+++ accounts/views.py
@@ -4,17 +4,18 @@ from rest_framework.response import Response
 from datetime import datetime
 from django.http import HttpResponse, JsonResponse
 
+
 # Create your views here.
 
 
 def render_form(request):
     if request.method == 'POST' and request.FILES:
         headers = {
-        "cache-control": "no-cache",
+            "cache-control": "no-cache",
         }
-        
+
         formdata = {}
-        
+
         # formdata['logo'] = request.FILES['logo'].file.getvalue() 
         files = {'logo': request.FILES['logo']}
         formdata['brand_name'] = request.POST.get('brand_name')
@@ -23,18 +24,16 @@ def render_form(request):
         formdata['location'] = request.POST.get('location')
         formdata['promotional_sentence'] = request.POST.get('promotional_sentence')
         host = request.META['HTTP_HOST']
-        
-        
 
-        url ='http://'+host+'/api/qrcode/'
-        res = requests.post(url, data = formdata, files=files)
+        url = 'http://' + host + '/api/qrcode/'
+        res = requests.post(url, data=formdata, files=files)
         res_data = res.json()
 
         upload_to_remote_db(res_data)
-        context ={
+        context = {
             'qrcode': res_data['qr_code'],
             'link': res_data['url']
-            
+
         }
 
         request.session['form_link'] = res_data['url']
@@ -42,62 +41,64 @@ def render_form(request):
 
     return render(request, 'form.html')
 
+
 def render_qrcode(request):
-    return render(request,'qrcode.html')
+    return render(request, 'qrcode.html')
 
 
 def render_iframe(request):
     context = {
-        'url_link':request.session['form_link']
+        'url_link': request.session['form_link']
     }
-    return render(request, 'iframe.html',context )
+    return render(request, 'iframe.html', context)
+
 
 def get_event_id():
-    dd=datetime.now()
-    time=dd.strftime("%d:%m:%Y,%H:%M:%S")
-    url="https://100003.pythonanywhere.com/event_creation"
-    data={"platformcode":"FB" ,"citycode":"101","daycode":"0",
-                    "dbcode":"pfm" ,"ip_address":"192.168.0.41",
-                    "login_id":"lav","session_id":"new",
-                    "processcode":"1","regional_time":time,
-                    "dowell_time":time,"location":"22446576",
-                    "objectcode":"1","instancecode":"100051","context":"afdafa ",
-                    "document_id":"3004","rules":"some rules","status":"work"
-                    }
+    dd = datetime.now()
+    time = dd.strftime("%d:%m:%Y,%H:%M:%S")
+    url = "https://100003.pythonanywhere.com/event_creation"
+    data = {"platformcode": "FB", "citycode": "101", "daycode": "0",
+            "dbcode": "pfm", "ip_address": "192.168.0.41",
+            "login_id": "lav", "session_id": "new",
+            "processcode": "1", "regional_time": time,
+            "dowell_time": time, "location": "22446576",
+            "objectcode": "1", "instancecode": "100051", "context": "afdafa ",
+            "document_id": "3004", "rules": "some rules", "status": "work"
+            }
 
-
-    r=requests.post(url,json=data)
+    r = requests.post(url, json=data)
     return r.text
+
 
 def upload_to_remote_db(data):
     # print('sanity check')
     # print(data)
     url = "http://100002.pythonanywhere.com/"
-    #searchstring="ObjectId"+"("+"'"+"6139bd4969b0c91866e40551"+"'"+")"
+    # searchstring="ObjectId"+"("+"'"+"6139bd4969b0c91866e40551"+"'"+")"
     payload = {
-    "cluster": "nps",
+        "cluster": "nps",
 
-    "database": "voc_survey",
+        "database": "voc_survey",
 
-    "collection": "client_voc_data",
+        "collection": "client_voc_data",
 
-    "document": "client_voc_data",
+        "document": "client_voc_data",
 
-    "team_member_ID": "76888881",
+        "team_member_ID": "76888881",
 
-    "function_ID": "ABCDE",
+        "function_ID": "ABCDE",
 
-    "command": "insert",
+        "command": "insert",
 
-    "field": data,
-    "update_field": {
-        "order_nos": 21
-    },
-    "platform": "bangalore"
+        "field": data,
+        "update_field": {
+            "order_nos": 21
+        },
+        "platform": "bangalore"
     }
     headers = {
-    'Content-Type': 'application/json'
+        'Content-Type': 'application/json'
     }
 
-    response = requests.post( url, headers=headers, json=payload)
+    response = requests.post(url, headers=headers, json=payload)
     print(response.text)
