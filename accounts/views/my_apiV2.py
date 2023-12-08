e0817bdcd9306867e9690d90f436a6bc9a255527
dennismnyaga <murildennis7@gmail.com>
2023-12-08 15:34:35 +0300

    updated the check status of survey participants limit

--- accounts/views/my_apiV2.py
+++ accounts/views/my_apiV2.py
@@ -12,6 +12,7 @@ from django.shortcuts import get_object_or_404
 import ast
 from django.conf import settings
 
+
 import requests
 from urllib.parse import urlparse, parse_qs
 
@@ -45,6 +46,7 @@ class GetDowellSurvey(APIView):
         return JsonResponse({"data": "Kindly use a POST request instead of GET"})
 
     def post(self, request, format=None):
+        
         current_date = datetime.now()
         dates = current_date.date()
         myDict = request.data
@@ -56,10 +58,10 @@ class GetDowellSurvey(APIView):
         try:
             api_key = request.query_params.get('api_key')
             print('This is the params api', api_key)
-            process_api_response = api_key
-            if process_api_response == '76092219-c570-4c86-88f0-efa63966e06b':
-            # process_api_response = processApikey(api_key)
-            # if process_api_response.status_code == 200:
+            # process_api_response = api_key
+            # if process_api_response == '76092219-c570-4c86-88f0-efa63966e06b':
+            process_api_response = processApikey(api_key)
+            if process_api_response.status_code == 200:
                 print('This is the api_key response', process_api_response)
                 company_id = myDict['company_id']
                 formdata['logo'] = myDict['logo']
@@ -72,12 +74,20 @@ class GetDowellSurvey(APIView):
                 formdata["username"] = myDict["username"]
                 formdata["name"] = myDict["name"]
                 formdata["email"] = myDict["email"]
-                # formdata["participantsLimit"] = myDict["participantsLimit"]
-                formdata["participantsLimit"] = myDict.get("participantsLimit", "{}")
+                formdata["participantsLimit"] = myDict["participantsLimit"]
+                # formdata["participantsLimit"] = myDict.getlist("participantsLimit")
+                # print('type of participants limit ', myDict.getlist("participantsLimit"))
+                # '''this is it'''
+                # participants_limit_list = formdata["participantsLimit"]
+                # print('regon type ', type(formdata["region"] ))
+                # regions_list = formdata["region"]
                 
-                # participants_limit_str = myDict.get("participantsLimit", "{}")
-                # participants_limit_dict = json.loads(participants_limit_str)
-                # formdata["participantsLimit"] = json.dumps(participants_limit_dict)  # Save it as a JSON string
+                # participants_limit_values = participants_limit_list[0].split(', ')
+                # regions_values = regions_list[0].split(', ')
+
+                # # Create the dictionary
+                # participants_limit_dict = {region.strip(): int(limit) for region, limit in zip(regions_values, participants_limit_values)}
+                # print('this is the dict', participants_limit_dict)
             
                 
                 
@@ -103,6 +113,7 @@ class GetDowellSurvey(APIView):
                 
                 print('Serialize this ', formdata)
                 serializer = CreateQrCodeSerializerV2(data=formdata)
+                print('Over here!')
                 if serializer.is_valid():
                     res = serializer.save()
                     print("this is the res data here", res)
@@ -110,11 +121,50 @@ class GetDowellSurvey(APIView):
                     res_data = serializer.data
 
                     upload_to_remote_db(res_data)
+                    
+                    
+                    '''================================='''
+                    
+                    
+                    context = {
+                        'qrcode': res_data['qr_code'],
+                        'link': 'https://'+settings.HOSTNAME+'/iframe?survey_id=' + str(res_data['id'])
 
+                    }
+                    
+                    qrcode_type = "Link"
+                    quantity = 1
+                    company_id = company_id
+                    link = 'https://'+settings.HOSTNAME + \
+                        '/iframe?survey_id=' + str(res_data['id'])
+                    description = res_data['promotional_sentence']
+                    created_by = res_data['username']
 
-                    return Response(res_data, status=status.HTTP_200_OK)
+                    qrcode_url = 'https://www.qrcodereviews.uxlivinglab.online/api/v2/qr-code/?api_key=' + api_key
+                    payload = {"qrcode_type": qrcode_type,
+                            "quantity": quantity,
+                            "company_id": company_id,
+                            # "logo":logo,
+                            "link": link,
+                            "description": description,
+                            "created_by": created_by}
+                    headers = {"Content-Type": "multipart/form-data"}
+
+                    print("files === ", files)
+                    res = requests.post(qrcode_url, data=payload)
+                    # res = {"qr_code_generator_response": res}
+                    print("res === ", res)
+                    print("res.text === ", res.text)
+                    print("type res.text === ", type(res.text))
+                    res_obj = json.loads(res.text)
+                    print("res_obj === ", res_obj)
+                    
+                    
+                    '''================================='''
+
+                    return Response(res_obj, status=status.HTTP_200_OK)
                 else:
-                    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                    return Response({"success": False, "error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
             else:
                 return Response("API Key validation failed.", status=status.HTTP_400_BAD_REQUEST)
 
@@ -163,10 +213,10 @@ class GetDowellSurvey(APIView):
                     formdata[key] = value
 
             api_key = request.query_params.get('api_key')
-            # process_api_response = api_key
-            # if process_api_response == '504a51bf-c483-4ac5-b2dd-4f209eabcbf8':
-            process_api_response = processApikey(api_key)
-            if process_api_response.status_code == 200:
+            process_api_response = api_key
+            if process_api_response == '504a51bf-c483-4ac5-b2dd-4f209eabcbf8':
+            # process_api_response = processApikey(api_key)
+            # if process_api_response.status_code == 200:
                 company_id = formdata.get('company_id')
                 description = formdata.get('description')
                 qrcode_color = formdata.get('qrcode_color')
@@ -177,7 +227,7 @@ class GetDowellSurvey(APIView):
                 host = request.META['HTTP_HOST']
 
                 brand_name = formdata.get("brand_name")
-                service = formdata.getlist("service")
+                service = formdata.get("service")
                 url = formdata.get("url")
                 country = formdata.get("country")
                 
@@ -200,10 +250,20 @@ class GetDowellSurvey(APIView):
                 name = formdata.get("name")
                 email = formdata.get("email")
                 link = formdata.get('link')
-                participantsLimit = formdata("participantsLimit")
+                participantsLimit = formdata.get("participantsLimit")
 
-                formdata["start_date"] = my_date(myDict["start_date"])
-                formdata["end_date"] = my_date(myDict["end_date"])
+                # formdata["start_date"] = my_date(myDict["start_date"])
+                # formdata["end_date"] = my_date(myDict["end_date"])
+                
+                
+                start_date = formdata.get("start_date")
+                if start_date:
+                    formdata["start_date"] = my_date(start_date)
+                    
+                end_date = formdata.get("end_date")
+                if end_date:
+                    formdata["end_date"] = my_date(end_date)
+
 
                 print('form data printier', formdata)
 
@@ -252,14 +312,23 @@ class ExtractAndFetchSurvey(APIView):
         # current_date = datetime.datetime.now()
         current_date = datetime.now()
         dates = current_date.date()
-        print('Am called')
+        
         link = request.data.get("link")
+        parsed_url = urlparse(link)
+
+        # Extract the survey_id from the query parameters
+        query_params = parse_qs(parsed_url.query)
+        survey_id = query_params.get('survey_id', [None])[0]
+
+        print("Survey ID:", survey_id)
+        print('Am called')
+        
         region = request.data.get("region")
         # region = request.data.get("region")
         print('This is the region ', region)
         print('This is the link ', link)
         parts = link.split("/")
-        extractedID = parts[-1]
+        extractedID = survey_id
         print('table id is', extractedID)
         if not link:
             return Response({"message": "Link is required"}, status=status.HTTP_400_BAD_REQUEST)
@@ -269,9 +338,10 @@ class ExtractAndFetchSurvey(APIView):
 
         if extractedID is not None:
             # Fetch the survey based on the extracted ID
+            
             survey = get_object_or_404(QrCodeV2, pk=extractedID)
             # participants_limit = survey.participantsLimit.get(region)
-            # print('This is it ', participants_limit)
+            print('This is it ', survey)
             qr_code_v2 = get_object_or_404(QrCodeV2, pk=extractedID)
             coordinator = SurveyCoordinator.objects.filter(survey=qr_code_v2).first()
             print('This is the cordinator ', coordinator)
@@ -314,27 +384,30 @@ class ExtractAndFetchSurvey(APIView):
             
             if survey.start_date <= dates <= survey.end_date:
                 participants_limit_str =  coordinator.survey.participantsLimit
+                print('Participant ', participants_limit_str)
                 coordinator_participants = coordinator.participants
-                print('This is the cordinator participants ', coordinator_participants)
-                participants_limit_dict = ast.literal_eval(participants_limit_str)
-                print('partsLimit ', participants_limit_dict)
+                print('This is the cordinator participants ', type( coordinator_participants))
+                # participants_limit_dict = ast.literal_eval(participants_limit_str)
+                # print('partsLimit ', participants_limit_dict)
                 
                 
-                if region in participants_limit_dict and participants_limit_dict[region] >= 1:
-                    region_value = participants_limit_dict[region]
-                    region_values = participants_limit_dict
-                    print('This is the regions ', region_values)
-                    if participants_limit_dict[region] >= 1:
-                        print('You can proceed!')
-                    else:
-                        print('You cant proceed')
-                    print(f"{region} exists with a value of {region_value}")
+                # if region in participants_limit_dict and participants_limit_dict[region] >= 1:
+                if coordinator_participants.get(region, 0) >= 1:
+                    
+                    # region_value = coordinator_participants.get(region, 0)
+                    # region_values = participants_limit_dict
+                    # print('This is the regions ', region_values)
+                    # if participants_limit_dict[region] >= 1:
+                    #     print('You can proceed!')
+                    # else:
+                    #     print('You cant proceed')
+                    # print(f"{region} exists with a value of {region_value}")
                     response_data = {
                     "isSuccess": True,
                     "message": "Survey can be conducted",
                     "survey_data": {
                         "region for survey": region,
-                        "participantsLimit": survey.participantsLimit,
+                        "participantsLimit": coordinator_participants,
                     }}
                     return Response(response_data, status=status.HTTP_200_OK)
                 else:
@@ -383,11 +456,18 @@ class SurveyCounter(APIView):
         
         print('Am called here')
         link = request.data.get("link")
+        # link = request.data.get("link")
         region = request.data.get("region")
-        print('This is the region ', region)
+        # print('This is the region ', region)
         print('This is the link ', link)
-        parts = link.split("/")
-        extractedID = parts[-1]
+        # parts = link.split("/")
+        # extractedID = parts[-1]
+        parsed_url = urlparse(link)
+
+        # Extract the survey_id from the query parameters
+        query_params = parse_qs(parsed_url.query)
+        survey_id = query_params.get('survey_id', [None])[0]
+        extractedID = survey_id
         print('table id is', extractedID)
         if not link:
             return Response({"message": "Link is required"}, status=status.HTTP_400_BAD_REQUEST)
@@ -396,34 +476,42 @@ class SurveyCounter(APIView):
 
         if extractedID is not None:
             survey = get_object_or_404(QrCodeV2, pk=extractedID)
-            
+            print('This is the survey ', survey)
             qr_code_v2 = get_object_or_404(QrCodeV2, pk=extractedID)
             coordinator = SurveyCoordinator.objects.filter(survey=qr_code_v2).first()
+            print('This cordinator data ', coordinator )
             
-            participants_limit = survey.participantsLimit
+            participants_limit = coordinator.participants
             print('This are the participants limit ', participants_limit)
             print("This is the type of participants ", type(participants_limit))
-            participants_limit_dict = ast.literal_eval(participants_limit)
-            print('This is type of ', participants_limit_dict)
-            print('partsLimit ', participants_limit)
+            # participants_limit_dict = ast.literal_eval(participants_limit)
+            # print('This is type of ', participants_limit_dict)
+            # print('partsLimit ', participants_limit)
             
-            if region in participants_limit_dict:
-                region_value = int(participants_limit_dict[region])
+            if region in participants_limit:
+                region_value = int(participants_limit.get(region, 0))
                 print(f"{region} exists with a value of {region_value}")
                 
-                
-                participants_limit_dict[region] -= 1
-                survey.participantsLimit = str(participants_limit_dict)
-                survey.save()
-                print('Decremented survey ', str(participants_limit_dict))
+                if region_value >= 1:
+                    # survey_value = participants_limit.get(region, 0)
+                    # survey_value -= 1
+                    # survey.participantsLimit = survey_value
+                    # survey.save()
+                    # participants_limit[region] = region_value - 1
+                    # survey.participantsLimit = participants_limit
+                    coordinator.participants[region] = region_value - 1
+                    coordinator.save()
+                    # kk = survey.save()
+                    # print('Saved ', coordinator.save())
+                    print('Decremented survey ', (participants_limit[region]))
             else:
                 print(f"{region} does not exist in the dictionary")
             response_data = {
                 "isSuccess": True,
-                "message": "Survey fetched successfully",
+                "message": "Survey counted successfully",
                 "survey_data": {
                     "brand_name": survey.brand_name,
-                    "participantsLimit": survey.participantsLimit,
+                    "participantsLimit": participants_limit,
                 }
             }
 
--- api/serializers.py
+++ api/serializers.py
@@ -173,64 +173,10 @@ class CreateQrCodeSerializerV2(serializers.ModelSerializer):
         fields = '__all__'
 
     def create(self, validated_data):
+        print('Over here bro!')
         try:
             new_qrcode = QrCodeV2.objects.create(**validated_data)
-            # Logo_link = validated_data.pop('logo')
-            # filename = Logo_link.name
-
-            # logo = Image.open(Logo_link)
-            # basewidth = 100
-
-            # wpercent = (basewidth/float(logo.size[0]))
-            # hsize = int((float(logo.size[1])*float(wpercent)))
-            # logo = logo.resize((basewidth, hsize), Image.ANTIALIAS)
-
-
-            # QRcode = qrcode.QRCode(
-            #     error_correction=qrcode.constants.ERROR_CORRECT_H
-            # )
-            # host = settings.HOSTNAME
-
-            # # url = validated_data.pop('url')
-            # survey_url = validated_data['url']
-
-            # url = 'https://' + host + '/iframe?survey_id='+ str(new_qrcode.id)
-            # print("url----------------------------->"+url)
-
-
-            # QRcode.add_data(url)
-
-
-            # QRcode.make()
-
-
-            # QRcolor = 'Black'
-
-            # # adding color to QR code
-            # QRimg = QRcode.make_image(
-            #     fill_color=QRcolor, back_color="white").convert('RGB')
-
-            # # set size of QR code
-            # pos = ((QRimg.size[0] - logo.size[0]) // 2,
-            #     (QRimg.size[1] - logo.size[1]) // 2)
-
-            # # box = Image.open('background.png')
-            # # QRimg.paste(box, pos)
-            # QRimg.paste(logo,pos)
-
-            # # QRimg.show()
-
-            # # save the QR code generated
-            # QRimg.save(settings.MEDIA_ROOT+'/company_qrcode/'+filename)
-            # filepath = 'company_qrcode/'+filename
-            # new_qrcode.qr_code = filepath
-            # with open(settings.MEDIA_ROOT+'/company_qrcode/'+filename, "rb") as image2string:
-            #     converted_string = base64.b64encode(image2string.read())
-
-
-
-            # new_qrcode.image = converted_string
-            # new_qrcode.event_id = get_event_id()
+            print('You got')
             new_qrcode.save()
             print("This is the validated data in serializer to be save", new_qrcode)
             return new_qrcode
@@ -244,7 +190,7 @@ class CreateQrCodeSerializerV2(serializers.ModelSerializer):
             
 class UpdateQrCodeSerializerV2(serializers.ModelSerializer):
     class Meta:
-        model = QrCode
+        model = QrCodeV2
         fields = '__all__'
         
         
@@ -279,5 +225,5 @@ class UpdateQrCodeSerializerV2(serializers.ModelSerializer):
 
 class ListQrCodeSerializer(serializers.ModelSerializer):
     class Meta:
-        model = QrCode
+        model = QrCodeV2
         fields = '__all__'
